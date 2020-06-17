4462b59cea4081f48e902b18e8d973f836079775
Joe <jspector906@gmail.com>
2020-06-16 20:41:10 -0500

    project work

---
+++ components/views/index.js
@@ -0,0 +1,6 @@
+export { default as Home } from "./Home"
+export { default as News } from "./News"
+export { default as Pics } from "./Pics"
+export { default as Vids } from "./Vids"
+export { default as Resources } from "./Resources"
+export { default as GiftShop } from "./GiftShop"
---
+++ index.js
@@ -0,0 +1,26 @@
+function render(){
+    document.querySelector("#root").innerHTML = `<p> i work</p>`
+}
+
+import { Header, Nav, Main, Footer } from "./components";
+import * as state from "./store";
+import Navigo from "navigo";
+// import axios from "axios";
+// import {makeAPI} from './components/views/Bio'
+const router = new Navigo(window.location.origin);
+router
+  .on({
+    ":page": params => render(state[params.page]),
+    "/": () => render(state.Home)
+  })
+  .resolve();
+  function render(st = state.Home) {
+      document.querySelector("#root").innerHTML = `
+      ${Header(st)}
+      ${Nav(state.Links)}
+      ${Main(st)}
+      ${Footer()}
+      `;
+      router.updatePageLinks();
+    }
+    render();
